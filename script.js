de9b495dd81580abdb8e3d033258e2ab84d86ffd
czirkel <clzirkel@gmail.com>
2016-02-01 18:14:26 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// arrays
+var firstArray = ["Photography", "Travel", "Music", "Beach", "City Night Life", "Be With Friends and Family", "Crafting"];
+console.log(firstArray[0]);
+console.log(firstArray[6]);
+// for loop through an array
+for (var i = 0; i < firstArray.length; i++) {
+	console.log("Array item number", i, firstArray[i]);
+}
